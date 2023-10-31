e0f374d4ca10f480849c0b093b070382b78cab98
鲲尘 <kunchen.xww@alibaba-inc.com>
2023-10-31 17:27:28 +0800

    feat: loader structure

--- crates/loader_compilation/src/lib.rs
+++ crates/loader_compilation/src/lib.rs
@@ -1,21 +1,19 @@
-use std::sync::Arc;
-use rspack_core::{rspack_sources::SourceMap, LoaderRunnerContext, Mode};
+use rspack_ast::RspackAst;
+use rspack_core::{rspack_sources::SourceMap, LoaderRunnerContext};
 use rspack_loader_runner::{Identifiable, Identifier, Loader, LoaderContext};
 use rspack_error::{
-  internal_error, Diagnostic, DiagnosticKind, Error, InternalError, Result, Severity,
-  TraceableError,
+  internal_error, Result,
 };
-use swc_core::{
-  base::{
-    Compiler,
-    config::{InputSourceMap, Options},
-    try_with_handler
-  },
-  common::{FilePathMapping,GLOBALS, FileName, comments::SingleThreadedComments},
-  ecma::transforms::base::pass::noop
+use swc_core::base::config::{InputSourceMap, Options, OutputCharset};
+use rspack_plugin_javascript::{
+  ast::{self, SourceMapConfig},
+  TransformOutput,
 };
+mod compiler;
 mod transform;
+
 use transform::*;
+use compiler::{SwcCompiler, IntoJsAst};
 pub struct CompilationLoader {
   identifier: Identifier,
 }
@@ -43,8 +41,6 @@ impl Loader<LoaderRunnerContext> for CompilationLoader {
     let Some(content) = std::mem::take(&mut loader_context.content) else {
       return Err(internal_error!("No content found"));
     };
-
-    let compiler = Compiler::new(Arc::from(swc_core::common::SourceMap::new(FilePathMapping::empty())));
     // TODO: init loader with custom options.
     let mut swc_options = Options::default();
 
@@ -56,31 +52,57 @@ impl Loader<LoaderRunnerContext> for CompilationLoader {
       }
     }
 
-    GLOBALS.set(&Default::default(), || {
-      try_with_handler(compiler.cm.clone(), Default::default(), |handler| {
-        compiler.run(|| {
-          let content = content.try_into_string()?;
-          let fm = compiler.cm.new_source_file(FileName::Real(resource_path.clone()), content.clone());
-          let comments = SingleThreadedComments::default();
-          let transform_options = SwcPluginOptions {
-            keep_export: None,
-            remove_export: None,
-          };
-          let out = compiler.process_js_with_custom_pass(
-            fm,
-            None,
-            handler,
-            &swc_options,
-            comments, |_| {
-              transform(transform_options)
-            },
-            |_| noop(),
-          )?;
-          Ok(())
-        })
-      })
+    let devtool = &loader_context.context.options.devtool;
+    let source = content.try_into_string()?;
+    let compiler = SwcCompiler::new(resource_path.clone(), source.clone(), swc_options)?;
+
+    let transform_options = SwcPluginOptions {
+      keep_export: None,
+      remove_export: None,
+    };
+    let built = compiler.parse(None, |_| {
+      transform(transform_options)
     })?;
-    
+
+    let codegen_options = ast::CodegenOptions {
+      target: Some(built.target),
+      minify: Some(built.minify),
+      ascii_only: built
+        .output
+        .charset
+        .as_ref()
+        .map(|v| matches!(v, OutputCharset::Ascii)),
+      source_map_config: SourceMapConfig {
+        enable: devtool.source_map(),
+        inline_sources_content: true,
+        emit_columns: !devtool.cheap(),
+        names: Default::default(),
+      },
+      keep_comments: Some(true),
+    };
+    let program = compiler.transform(built)?;
+    let ast = compiler.into_js_ast(program);
+
+    // If swc-loader is the latest loader available,
+    // then loader produces AST, which could be used as an optimization.
+    if loader_context.loader_index() == 0
+      && (loader_context
+        .current_loader()
+        .composed_index_by_identifier(&self.identifier)
+        .map(|idx| idx == 0)
+        .unwrap_or(true))
+    {
+      loader_context
+        .additional_data
+        .insert(RspackAst::JavaScript(ast));
+      loader_context.additional_data.insert(codegen_options);
+      loader_context.content = Some("".to_owned().into())
+    } else {
+      let TransformOutput { code, map } = ast::stringify(&ast, codegen_options)?;
+      loader_context.content = Some(code.into());
+      loader_context.source_map = map.map(|m| SourceMap::from_json(&m)).transpose()?;
+    }
+
     Ok(())
   }
 }
