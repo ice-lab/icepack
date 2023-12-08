e5cbd36052c6f74b8c6a374abdbe5b8e10ee8666
ClarkXia <xiawenwu41@gmail.com>
2023-12-08 14:41:34 +0800

    fix: add built loader of react refresh

--- crates/binding_options/src/options/raw_module.rs
+++ crates/binding_options/src/options/raw_module.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use rspack_core::BoxLoader;
 use rspack_loader_sass::SASS_LOADER_IDENTIFIER;
 use rspack_loader_swc::SWC_LOADER_IDENTIFIER;
+use rspack_loader_react_refresh::REACT_REFRESH_LOADER_IDENTIFIER;
 use loader_compilation::COMPILATION_LOADER_IDENTIFIER;
 
 pub fn get_builtin_loader(builtin: &str, options: Option<&str>) -> BoxLoader {
@@ -21,6 +22,12 @@ pub fn get_builtin_loader(builtin: &str, options: Option<&str>) -> BoxLoader {
         }),
       )
       .with_identifier(builtin.into()),
+    );
+  }
+
+  if builtin.starts_with(REACT_REFRESH_LOADER_IDENTIFIER) {
+    return Arc::new(
+      rspack_loader_react_refresh::ReactRefreshLoader::default().with_identifier(builtin.into()),
     );
   }
 
