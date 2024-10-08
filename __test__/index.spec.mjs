a5a7b3f787e0376a19e72075226d485e788e485d
ClarkXia <xiawenwu41@gmail.com>
2024-10-08 15:03:41 +0800

    fix: add lock file to fix the mismatch version of hash

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -3,7 +3,7 @@ import test from 'ava'
 import { getCssModulesLocalIdent } from '../index.js'
 
 test('template [hash]', (t) => {
-  t.is(getCssModulesLocalIdent('src/pages/index.module.css', 'test' ,'[hash]'), 'c25864290c9e89ea1553');
+  t.is(getCssModulesLocalIdent('src/pages/index.module.css', 'test' ,'[hash]'), '_58deea1c54f94c19c993');
 })
 test('template [path][name][ext]__[local]', (t) => {
   t.is(getCssModulesLocalIdent('src/pages/index.module.css', 'test' ,'[path][name][ext]__[local]'), 'src-pages-index-module-css__test');
